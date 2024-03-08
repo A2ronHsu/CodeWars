c47d9c7eb8655b991c19c8a5f75557345214af82
Aaron <aaronbee1990@gmail.com>
2024-03-08 17:26:45 -0300

    i'm not going to come up with the heap algorithm alone

--- JavaScript/Rank up/240215So Many Permutations.js
+++ JavaScript/Rank up/240215So Many Permutations.js
@@ -1,2 +1,11 @@
 //In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.\
 //https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/javascript
+function swap(swaparr, indexA,indexB){
+    [swaparr[indexA], swaparr[indexB]] = [swaparr[indexB], swaparr[indexA]]
+}
+
+
+function permutations(string, arr = string.split(''), i=0, j=1, result = []) {
+
+}
+console.log(permutations('abcd'));
