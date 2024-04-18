4704b1df44cb80cd856ac6885869fc20ebb12d85
Aaron <aaronbee1990@gmail.com>
2024-04-18 15:19:50 -0400

    first time doing functional programing

---
+++ JavaScript/Fundamentals/20240418Calculating with finctions.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/javascript
+
+function zero(fn) { return 0}
+function one(fn) { return 1}
+function two(fn) { return 2}
+function three(fn) { return 3}
+function four(fn) { return 4}
+function five(fn) { return 5}
+function six(fn) { return 6}
+function seven(fn) { return 7}
+function eight(fn) {return 8}
+function nine(fn) { return 9}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
