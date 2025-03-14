aa463a102be238981ae5da114e7534e1eee190db
Aaron <aaronbee1990@gmail.com>
2025-03-14 16:03:05 -0300

    refactor roman numerals encoder

--- TypeScript/20250205-Roman Numeral Encoder.ts
+++ TypeScript/20250205-Roman Numeral Encoder.ts
@@ -1,43 +1,48 @@
 //https://www.codewars.com/kata/51b62bf6a9c58071c600001b/train/typescript
-export function solution(number: number): string {
-   let input = number;
-   const oneArray = [1000, 100, 10, 1];
-   const fiveArray = [500, 50, 5]
-   const romOneArray = ['M', 'C', 'X','I'];
-   const romFiveArray = ['D', 'L', 'V']
-   let result = '';
+// export function solution(input: number): string {
+//    let result='';
+//    let number = input;
+//    let decArr = [1000,500,100,50,10,5,1];
+//    let romArr = ['M','D','C','L','X','V','I'];
 
-   let i = 0;
-   let decCurry = 0;
-   let romCurry = 0;
+//    for(let i = 0; i < decArr.length; i+=2){
+//       const repeatParam = Math.trunc(number/decArr[i]);
+//       if(number>0){
+//          if(repeatParam == 9){
+//             result+= romArr[i]+romArr[i-2];
+//          }else if(repeatParam == 4){
+//             result+= romArr[i]+romArr[i-1];
+//          }else if(repeatParam < 5){
+//             result+= romArr[i].repeat(repeatParam);
+//          }else if(repeatParam > 4){
+//             result+= romArr[i-1]+romArr[i].repeat(repeatParam-5);
+//          }
 
-   while (input > 1){
-      let identicalCharNumber = Math.trunc(input/oneArray[i]);
-      if(identicalCharNumber){
-         if(identicalCharNumber<=3){
-            result += romOneArray[i].repeat(identicalCharNumber);
-         }
-         if(identicalCharNumber === 4){
-            result += romOneArray[i+1]+romFiveArray[i];
-         }
-         if(identicalCharNumber < 9 && identicalCharNumber > 4){
-            result += romFiveArray[i]+romOneArray[i+1];
-         }
-         if(identicalCharNumber === 9){
-            result += romOneArray[i+1]+romOneArray[i];
-         }
+//          number -= repeatParam*decArr[i];
+//       }
+      
+//    } 
 
-         input -= identicalCharNumber*oneArray[i];
-      }
-      console.log(i,input, identicalCharNumber,oneArray[i]);
-      i++;
-     
-   }
+//    return result;
+// }
 
-   if(input == 1) result+= '1';
+function solution(number: number):string{
+   const map = {
+      1000 : 'M',
+      900 : 'CM',
+      500 : 'D',
+      100 : 'C',
+      90 : 'XC',
+      50 : 'L',
+      10 : 'X',
+      9 : 'IX',
+      5 : 'V',
+      1: 'I'
+   };
+
    
-
-   return result;
+   return '';
 }
 
-console.log(solution(99));
+console.log(solution(3008));
+console.log(Math.trunc(3999/1000));
